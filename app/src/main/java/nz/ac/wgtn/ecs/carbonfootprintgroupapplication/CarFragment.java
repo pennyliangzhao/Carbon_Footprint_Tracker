34956c56b7895323ef3fba2d8a0d69d2c7b07f5c
Rhea Gorospe <gorospceas@myvuw.ac.nz>
2021-10-21 17:36:09 +1300

    TimePickerFragment DateTime on Car Fragment

--- app/src/main/java/nz/ac/wgtn/ecs/carbonfootprintgroupapplication/CarFragment.java
+++ app/src/main/java/nz/ac/wgtn/ecs/carbonfootprintgroupapplication/CarFragment.java
@@ -1,30 +1,50 @@
 package nz.ac.wgtn.ecs.carbonfootprintgroupapplication;
 
 import android.app.DatePickerDialog;
+import android.icu.text.DateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CarFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+
+public class CarFragment extends Fragment {
+    private EditText date;
+    private DatePickerDialog datePickerDialog;
+    private CarFragment carFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_car, container, false);
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_car, container, false);
+        date = view.findViewById(R.id.date);
+        carFragment = this;
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment(carFragment);
+                newFragment.show(getActivity().getSupportFragmentManager(), "ti" +
+                        "mePicker");
+            }
+        });
+        return view;
     }
 
-    public void calendarPicker(){
-
+    public void updateDateTime(int hourOfDay, int minute) {
+        date.setText(Integer.toString(hourOfDay));
     }
 
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
-    }
 }
+
---
+++ app/src/main/java/nz/ac/wgtn/ecs/carbonfootprintgroupapplication/TimePickerFragment.java
@@ -0,0 +1,44 @@
+package nz.ac.wgtn.ecs.carbonfootprintgroupapplication;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    private CarFragment carFragment;
+
+    public TimePickerFragment(CarFragment carFragment) {
+        this.carFragment = carFragment;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+       // CarFragment fragment = (CarFragment) this.getParentFragment();
+        carFragment.updateDateTime(hourOfDay, minute);
+    }
+
+}
