441d944f30e47cdb2e2336078e8868f5ce1523f1
Rhea Gorospe <gorospceas@myvuw.ac.nz>
2021-10-22 11:18:56 +1300

    Spinners added on CarFragment

--- app/src/main/java/nz/ac/wgtn/ecs/carbonfootprintgroupapplication/CarFragment.java
+++ app/src/main/java/nz/ac/wgtn/ecs/carbonfootprintgroupapplication/CarFragment.java
@@ -11,17 +11,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CarFragment extends Fragment {
     private Button date;
     private DatePickerDialog datePickerDialog;
     private CarFragment carFragment;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +36,22 @@ public class CarFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_car, container, false);
         date = view.findViewById(R.id.date);
         carFragment = this;
+
+        String [] values =
+                {"Diesel","Petrol","Hybrid","Full EV",};
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        String [] values2 =
+                {"Small","Compact SUV","Large SUV","Van",};
+        Spinner spinner2 = (Spinner) view.findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values2);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner2.setAdapter(adapter2);
+
+
         date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,7 +66,6 @@ public class CarFragment extends Fragment {
     public void updateDateTime(int hourOfDay, int minute) {
         date.setText(Integer.toString(hourOfDay));
     }
-
-
 }
+
 
