8f49da7a6ac69b564c4c8661e78277f1c979f342
Rhea Gorospe <gorospceas@myvuw.ac.nz>
2021-10-21 14:07:41 +1300

    fix errorbuild

--- app/src/main/java/nz/ac/wgtn/ecs/carbonfootprintgroupapplication/CarFragment.java
+++ app/src/main/java/nz/ac/wgtn/ecs/carbonfootprintgroupapplication/CarFragment.java
@@ -1,5 +1,6 @@
 package nz.ac.wgtn.ecs.carbonfootprintgroupapplication;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
-public class CarFragment extends Fragment {
+public class CarFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,6 +20,11 @@ public class CarFragment extends Fragment {
     }
 
     public void calendarPicker(){
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
     }
 }
