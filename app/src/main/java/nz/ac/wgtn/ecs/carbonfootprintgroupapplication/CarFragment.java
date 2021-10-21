b1baf1edcb5b66ef3029fbe68966456fffad8432
Rhea Gorospe <gorospceas@myvuw.ac.nz>
2021-10-22 11:41:40 +1300

    Errors fixed
    Car Fragment Spinners added
     - old push was with empty changes

--- app/src/main/java/nz/ac/wgtn/ecs/carbonfootprintgroupapplication/CarFragment.java
+++ app/src/main/java/nz/ac/wgtn/ecs/carbonfootprintgroupapplication/CarFragment.java
@@ -11,9 +11,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,6 +32,22 @@ public class CarFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_car, container, false);
         date = view.findViewById(R.id.date);
         carFragment = this;
+
+        String [] values =
+                {"Diesel","Petrol","Hybrid","Full EV",};
+        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        String [] values2 =
+                {"Small","Compact SUV","Large SUV","Van",};
+        Spinner spinner2 = (Spinner) view.findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values2);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner2.setAdapter(adapter2);
+
+
         date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,6 +57,7 @@ public class CarFragment extends Fragment {
             }
         });
         return view;
+
     }
 
     public void updateDateTime(int hourOfDay, int minute) {
