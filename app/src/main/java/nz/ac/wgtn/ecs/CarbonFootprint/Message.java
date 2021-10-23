68971cca605dc458a92daf31fd72d26a8f3655a1
Rhea Gorospe <gorospceas@myvuw.ac.nz>
2021-10-23 15:59:01 +1300

    Added dimes, Message and MyDbAdapter files in the version controller.

---
+++ app/src/main/java/nz/ac/wgtn/ecs/CarbonFootprint/Message.java
@@ -0,0 +1,11 @@
+package nz.ac.wgtn.ecs.CarbonFootprint;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Message  {
+    public static void message(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
