c787f1d1b3dcab809eb1d68927464dcd51c66efe
nunnsloui <nunnsloui@myvuw.ac.nz>
2021-10-23 19:00:06 +1300

    Added Food Record Page and Action Recorder Page

--- app/src/main/java/nz/ac/wgtn/ecs/CarbonFootprint/ActionRecorderPage.java
+++ app/src/main/java/nz/ac/wgtn/ecs/CarbonFootprint/ActionRecorderPage.java
@@ -1,51 +1,65 @@
 package nz.ac.wgtn.ecs.CarbonFootprint;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RadioButton;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class ActionRecorderPage extends BaseActivity {
+        private double totalActionPoints = 0;
+        private static final double CONSERVE_ENERGY_POINTS = 17.0;
+        private static final double RECYCLE_POINTS = 20.0;
+        private static final double BIKE_MORE_POINTS = 12.0;
+        private static final double LESS_DISPOSABLE_PACKAGING_POINTS = 13.0;
+        private static final double PLANT_TREE_POINTS = 30.0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_action_recorder_page);
-    }
+        private TextView textView;
 
-    public void onRadioButtonClicked(View view){
-        boolean checked = ((RadioButton) view).isChecked();
-        // Check which RadioButton was clicked
-        switch(view.getId()) {
-            case R.id.conserveEnergyBtn:
-                if (checked) {
-                   // Intent intent = new Intent(this, ___.class);
-                   // startActivity(intent);
-                }
-                break;
-            case R.id.recycleBtn:
-                if (checked) {
-                  //  Intent intent = new Intent(this, ___.class);
-                   // startActivity(intent);
-                }
-            case R.id.bikeMoreBtn:
-                if (checked) {
-                  //  Intent intent = new Intent(this, ___.class);
-                   // startActivity(intent);
-                }
-                break;
-            case (R.id.lessDisposablePackagingBtn):
-                if (checked) {
-                    //Intent intent = new Intent(this, ___.class);
-                   // startActivity(intent);
-                }
-                break;
-            case (R.id.plantTreeBtn):
-                if (checked) {
-                   // Intent intent = new Intent(this, ___.class);
-                   // startActivity(intent);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_action_recorder_page);
+            textView = findViewById(R.id.totalActionPoints);
+        }
 
-                }
+        public void onCheckboxClicked(View view) {
+            boolean checked = ((CheckBox) view).isChecked();
+            switch (view.getId()) {
+                case R.id.conserve_energy:
+                    if (checked) {
+                        totalActionPoints += CONSERVE_ENERGY_POINTS;
+                    } else {
+                        totalActionPoints -= CONSERVE_ENERGY_POINTS;
+                    }
+                case R.id.recycle:
+                    if (checked) {
+                        totalActionPoints += RECYCLE_POINTS;
+                    } else {
+                        totalActionPoints -= RECYCLE_POINTS;
+                    }
+                case R.id.bike_more:
+                    if (checked) {
+                        totalActionPoints += BIKE_MORE_POINTS;
+                    } else {
+                        totalActionPoints -= BIKE_MORE_POINTS;
+                    }
+                case R.id.less_disposable_packaging:
+                    if (checked) {
+                        totalActionPoints += LESS_DISPOSABLE_PACKAGING_POINTS;
+                    } else {
+                        totalActionPoints -= LESS_DISPOSABLE_PACKAGING_POINTS;
+                    }
+                case R.id.plant_tree:
+                    if (checked) {
+                        totalActionPoints += PLANT_TREE_POINTS;
+                    } else {
+                        totalActionPoints -= PLANT_TREE_POINTS;
+                    }
+                    textView.setText(Double.toString(totalActionPoints));
+            }
+        }
+
+        public void savePoints(View view) {
+            //textView.setText(Double.toString(totalFoodPoints));
         }
     }
-}
--- app/src/main/java/nz/ac/wgtn/ecs/CarbonFootprint/FoodRecordPage.java
+++ app/src/main/java/nz/ac/wgtn/ecs/CarbonFootprint/FoodRecordPage.java
@@ -2,12 +2,65 @@ package nz.ac.wgtn.ecs.CarbonFootprint;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class FoodRecordPage extends BaseActivity {
+    private double totalFoodPoints = 0;
+    private static final double HEAVY_MEAT_POINTS = 27.0;
+    private static final double MEDIUM_MEAT_POINTS = 12.0;
+    private static final double LIGHT_MEAT_POINTS = 8.0;
+    private static final double VEGETARIAN_POINTS = 3.0;
+    private static final double VEGAN_POINTS = 2.0;
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_record_page);
+        textView = findViewById(R.id.totalFoodPoints);
+    }
+
+    public void onCheckboxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.food_type_heavy_meat:
+                if (checked) {
+                    totalFoodPoints += HEAVY_MEAT_POINTS;
+                } else {
+                    totalFoodPoints -= HEAVY_MEAT_POINTS;
+                }
+            case R.id.food_type_medium_meat:
+                if (checked) {
+                    totalFoodPoints += MEDIUM_MEAT_POINTS;
+                } else {
+                    totalFoodPoints -= MEDIUM_MEAT_POINTS;
+                }
+            case R.id.food_type_light_meat:
+                if (checked) {
+                    totalFoodPoints += LIGHT_MEAT_POINTS;
+                } else {
+                    totalFoodPoints -= LIGHT_MEAT_POINTS;
+                }
+            case R.id.food_type_vegetarian:
+                if (checked) {
+                    totalFoodPoints += VEGETARIAN_POINTS;
+                } else {
+                    totalFoodPoints -= VEGETARIAN_POINTS;
+                }
+            case R.id.food_type_vegan:
+                if (checked) {
+                    totalFoodPoints += VEGAN_POINTS;
+                } else {
+                    totalFoodPoints -= VEGAN_POINTS;
+                }
+                textView.setText(Double.toString(totalFoodPoints));
+        }
+    }
+
+    public void savePoints(View view) {
+        //textView.setText(Double.toString(totalFoodPoints));
     }
 }
